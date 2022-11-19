4559e7c7fed8dd7cfdb63113552403c80e63cd24
Kenta Yamada <knt.yamada.800@gmail.com>
2022-11-19 12:05:03 +0900

    Implement sample component & unit test

---
+++ src/components/helloworld/helloworld.compnent.spec.tsx
@@ -0,0 +1,11 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { HelloWorldComponent } from "./helloworld.component";
+
+describe("HelloWorldComponent tests" , () => {
+    test("`Hello World`が描画されている", () => {
+        render(<HelloWorldComponent />);
+        screen.debug();
+        expect(screen.getByText("Hello World")).toBeInTheDocument();
+    });
+});
---
+++ src/components/helloworld/helloworld.component.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const HelloWorldComponent: React.FunctionComponent = () => {
+    return <p>Hello World</p>
+};
