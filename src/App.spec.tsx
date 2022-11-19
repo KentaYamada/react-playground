4b4575eba1852dccfc5c8fd5e7f3c2f1c62436be
Kenta Yamada <knt.yamada.800@gmail.com>
2022-11-19 12:28:54 +0900

    Implement event unit test

---
+++ src/App.spec.tsx
@@ -0,0 +1,16 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
+
+describe("App component tests", () => {
+    it("should icrement count on click" , async () => {
+        render(<App />);
+        expect(screen.findByText(/count is 0/i));
+
+        userEvent.click(screen.getByRole("button"));
+
+        expect(await screen.findByText(/count is 1/i)).not.toBeEmptyDOMElement();
+        screen.debug();
+    });
+});
