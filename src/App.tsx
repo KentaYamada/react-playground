0619f8cb6aa761593a7c91e62978b51f2d185e9a
Kenta Yamada <knt.yamada.800@gmail.com>
2022-11-21 11:13:43 +0900

    Setup emotion/react

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import { useState } from 'react'
+import { FunctionComponent, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { css } from '@emotion/react'
 
-function App() {
+const style = css({
+  color: "blue"
+});
+
+const App: FunctionComponent = () => {
   const [count, setCount] = useState(0)
 
   return (
@@ -15,7 +20,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 css={style}>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
